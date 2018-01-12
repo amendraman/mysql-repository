fe54c3fed2ad2136bad439d8cf48545c9da0ad9d
EAI <amansingh0624@gmail.com>
2018-01-12 17:36:01 +0530

    add osme

---
+++ aadharproject2018.sql
@@ -0,0 +1,126 @@
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.1.73-community
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+--
+-- Create schema aadharmuleproject
+--
+
+CREATE DATABASE IF NOT EXISTS aadharmuleproject;
+USE aadharmuleproject;
+
+--
+-- Definition of table `aadhar`
+--
+
+DROP TABLE IF EXISTS `aadhar`;
+CREATE TABLE `aadhar` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(45) NOT NULL DEFAULT '',
+  `phonenumber` int(10) unsigned NOT NULL DEFAULT '0',
+  `address` varchar(45) NOT NULL DEFAULT '',
+  `username` varchar(45) NOT NULL DEFAULT '',
+  `password` varchar(45) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `aadhar`
+--
+
+/*!40000 ALTER TABLE `aadhar` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aadhar` ENABLE KEYS */;
+
+--
+-- Create schema fullfledgedaadharproject
+--
+
+CREATE DATABASE IF NOT EXISTS fullfledgedaadharproject;
+USE fullfledgedaadharproject;
+
+--
+-- Definition of table `aadharinfo`
+--
+
+DROP TABLE IF EXISTS `aadharinfo`;
+CREATE TABLE `aadharinfo` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `FullName` varchar(100) NOT NULL,
+  `PhoneNumber` int(11) NOT NULL,
+  `BirthMark` varchar(100) NOT NULL,
+  `Username` varchar(100) NOT NULL,
+  `Pass` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `aadharinfo`
+--
+
+/*!40000 ALTER TABLE `aadharinfo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aadharinfo` ENABLE KEYS */;
+
+
+--
+-- Definition of table `address`
+--
+
+DROP TABLE IF EXISTS `address`;
+CREATE TABLE `address` (
+  `ID` int(11) NOT NULL,
+  `State` varchar(100) NOT NULL,
+  `District` varchar(100) NOT NULL,
+  `PostalCode` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `address`
+--
+
+/*!40000 ALTER TABLE `address` DISABLE KEYS */;
+/*!40000 ALTER TABLE `address` ENABLE KEYS */;
+
+
+--
+-- Definition of table `familydetails`
+--
+
+DROP TABLE IF EXISTS `familydetails`;
+CREATE TABLE `familydetails` (
+  `ID` int(11) NOT NULL,
+  `FatherName` varchar(30) NOT NULL,
+  `MotherName` varchar(30) NOT NULL,
+  `MartialStatus` varchar(30) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `familydetails`
+--
+
+/*!40000 ALTER TABLE `familydetails` DISABLE KEYS */;
+/*!40000 ALTER TABLE `familydetails` ENABLE KEYS */;
+
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
